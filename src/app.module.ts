83ac3c02d6ae7d59009af0c55d0075d79c136b16
shaunstone0 <shaunstone0@gmail.com>
2021-12-19 17:35:14 +0100

    HISTAP-4 - Created Typeorm Config Service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import {
+    ConfigModule as NestConfigModule,
+    ConfigService as NestConfigService,
+} from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WinstonModule } from 'nest-winston';
-import { WinstonConfigService } from './config/winston-config/winston-config.service';
-import { getOrmOptionsObject } from './config/utils/typeorm-utils';
 import { Connection } from 'typeorm';
-import { DateUtilsService } from './shared/utils/date-utils/date-utils.service';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeormConfigService } from './config/typeorm-config/typeorm-config.service';
+import { WinstonConfigService } from './config/winston-config/winston-config.service';
 import { DateUtilsModule } from './shared/utils/date-utils/date-utils.module';
 
 @Module({
     imports: [
         NestConfigModule.forRoot({ isGlobal: true }),
         TypeOrmModule.forRootAsync({
-            useFactory: async () => Object.assign(await getOrmOptionsObject()),
+            useClass: TypeormConfigService,
+            inject: [NestConfigService],
         }),
         WinstonModule.forRootAsync({
             useClass: WinstonConfigService,
---
+++ src/config/typeorm-config/typeorm-config.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService as NestConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+import { TypeormTypeNameEnum } from '../enums/config.enums';
+
+@Injectable()
+export class TypeormConfigService implements TypeOrmOptionsFactory {
+    public type: TypeormTypeNameEnum.MYSQL;
+    public host: string;
+    public port: number;
+    public username: string;
+    public password: string;
+    public database: string;
+    public entities: string;
+    public migrationsDirectory: string;
+
+    public TypeormTypeNameEnum = TypeormTypeNameEnum;
+
+    constructor(private nestConfigService: NestConfigService) {}
+
+    // This is the function that is called when this class is
+    // instantiated. This creates the Typeorm connection.
+
+    public createTypeOrmOptions(): TypeOrmModuleOptions {
+        this.getEnvironmentVariables();
+
+        return {
+            type: this.type,
+            host: this.host,
+            port: this.port,
+            username: this.username,
+            password: this.password,
+            database: this.database,
+            entities: [this.entities],
+            cli: {
+                migrationsDir: this.migrationsDirectory,
+            },
+        };
+    }
+    // Method to get all the environment variables. The Syntax used is from the next documentation.
+    // https://docs.nestjs.com/
+
+    private getEnvironmentVariables(): void {
+        const mysqlType = this.TypeormTypeNameEnum.MYSQL;
+        this.type = this.nestConfigService.get<string>(
+            'MYSQL_TYPE',
+        ) as typeof mysqlType;
+        this.host = this.nestConfigService.get<string>('MYSQL_HOST');
+        this.port = parseInt(
+            this.nestConfigService.get<string>('MYSQL_PORT'),
+            10,
+        );
+        this.username = this.nestConfigService.get<string>('MYSQL_USERNAME');
+        this.password = this.nestConfigService.get<string>('MYSQL_PASSWORD');
+        this.database = this.nestConfigService.get<string>('MYSQL_DATABASE');
+        this.entities = this.nestConfigService.get<string>('MYSQL_ENTITIES');
+        this.migrationsDirectory = this.nestConfigService.get<string>(
+            'CLI_MIGRATIONS_DIRECTORY',
+        );
+    }
+}
